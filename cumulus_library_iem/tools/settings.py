8c05393ad8f7b9c678e3ebc1384735a4c5042579
comorbidity <andrew.mcmurry@tch.harvard.edu>
2026-06-10 15:28:34 -0700

    settings.py added "ELASTIC_OUTPUT"

--- cumulus_library_iem/tools/settings.py
+++ cumulus_library_iem/tools/settings.py
@@ -2,6 +2,7 @@ import os
 
 CUMULUS_CUBE_AS_VIEW = int(os.environ.get("CUMULUS_CUBE_AS_VIEW") or 0)
 CUMULUS_CUBE_MIN_SUBJECTS = int(os.environ.get("CUMULUS_CUBE_MIN_SUBJECTS") or 1)
+ELASTIC_OUTPUT = os.environ.get("ELASTIC_OUTPUT")
 
 # print('###########################################################')
 # print('[Settings]')
@@ -11,5 +12,8 @@ CUMULUS_CUBE_MIN_SUBJECTS = int(os.environ.get("CUMULUS_CUBE_MIN_SUBJECTS") or 1
 # print()
 # print('Optional: Minimum number of patients per set size in CUBE data')
 # print('CUMULUS_CUBE_MIN_SUBJECTS', '=', CUMULUS_CUBE_MIN_SUBJECTS)
+# print()
+# print('Optional: Elastic search resluts (output) ')
+# print('ELASTIC_OUTPUT', '=', ELASTIC_OUTPUT)
 # print()
 # print('###########################################################')
